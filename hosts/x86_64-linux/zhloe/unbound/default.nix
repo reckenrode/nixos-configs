4305584d10c53ba07908536e679b40e29b2eef04
Randy Eckenrode <randy@largeandhighquality.com>
2021-03-12 21:54:32 -0500

    Enable ghttp2 support on Unbound
    Building with ghttp2 is required to enable DoH support.

--- hosts/x86_64-linux/zhloe/unbound/default.nix
+++ hosts/x86_64-linux/zhloe/unbound/default.nix
@@ -1,12 +1,26 @@
 { config, pkgs, ... }:
+
 {
+  nixpkgs.overlays = [
+    (final: prev: {
+      unbound_doh = prev.unbound.overrideAttrs (old: {
+        buildInputs = old.buildInputs ++ [ final.nghttp2 ];
+        configureFlags = old.configureFlags ++ [
+	  "--with-libnghttp2=${final.nghttp2.dev}"
+	];
+      });
+    })
+  ];
+
+  security.acme.certs."infra.largeandhighquality.com".postRun = "systemctl reload unbound.service";
+
   services.unbound = {
     allowedAccess = [ "192.168.238.1/24" "fe80::/64" ];
     interfaces = [ "192.168.238.1" "fe80::2e0:67ff:fe15:ced3%enp2s0" ];
     extraConfig = let
       certPath = config.security.acme.certs."infra.largeandhighquality.com".directory;
     in builtins.readFile (pkgs.substituteAll { src = ./unbound.conf; inherit certPath; });
-    package = pkgs.unstable.unbound;
+    package = pkgs.unbound_doh;
   };
 
   networking.nftables.ruleset = builtins.readFile ./unbound.nft;
