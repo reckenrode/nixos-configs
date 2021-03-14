b177a6229a7337cd16e00694af140c19d2e382a1
Randy Eckenrode <randy@largeandhighquality.com>
2021-03-14 15:44:08 -0400

    Allow coturn users to be specified in a file
    Coturn puts its users in a static file. This dirty hack appends them to the end of the configuration file. It needs to evaluate the coturn module separately to avoid an infinite recursion. This works because the two configs are never merged.

---
+++ hosts/x86_64-linux/web-host/foundryvtt/coturn-ext.nix
@@ -0,0 +1,37 @@
+{ config, lib, pkgs, modulesPath, ... }:
+
+let
+  args = {
+    inherit lib pkgs modulesPath;
+    config.services.coturn = config.services.coturn;
+  };
+  coturnModule = import (modulesPath + "/services/networking/coturn.nix") args;
+  execStart = coturnModule.config.content.systemd.services.coturn.serviceConfig.ExecStart;
+  configFile = lib.trivial.pipe execStart [
+    (lib.strings.splitString " ")
+    (lib.flip builtins.elemAt 2)
+  ];
+  runtimeConfigFile = "/run/turnserver/turnserver.conf";
+in {
+  options = {
+    services.coturn.static-users-file = lib.mkOption {
+      type = lib.types.nullOr lib.types.path;
+      description = ''
+        File containing a list of static users for coturn. The list must follow the format used in
+	turnserver.conf.
+      '';
+      default = null;
+    };
+  };
+
+  config = lib.mkIf (config.services.coturn ? static-users-file) {
+    systemd.services.coturn.preStart = ''
+      install -m 0600 -g turnserver -o turnserver ${configFile} ${runtimeConfigFile}
+      cat ${config.services.coturn.static-users-file} >> ${runtimeConfigFile}
+    '';
+  
+    systemd.services.coturn.serviceConfig.ExecStart = lib.mkOverride 0 ''
+      ${pkgs.coturn}/bin/turnserver -c ${configFile}
+    '';
+  };
+}
