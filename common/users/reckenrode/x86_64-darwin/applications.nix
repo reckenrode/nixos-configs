e0e9a9cd981ff32cfac06e73a0626d46ad4aac3e
Randy Eckenrode <randy@largeandhighquality.com>
2021-03-29 23:44:27 -0400

    Support verbose activation in copyApplications

--- common/users/reckenrode/x86_64-darwin/applications.nix
+++ common/users/reckenrode/x86_64-darwin/applications.nix
@@ -9,10 +9,11 @@
         pathsToLink = "/Applications";
       };
     in lib.hm.dag.entryAfter [ "writeBoundary" ] ''
+      appsSrc="${apps}/Applications/"
       baseDir="$HOME/Applications/Home Manager Apps"
       rsyncArgs="--archive --chmod=u+w --delete -L"
       mkdir -p "$baseDir"
-      $DRY_RUN_CMD ${pkgs.rsync}/bin/rsync $rsyncArgs ${apps}/Applications/ "$baseDir"
+      $DRY_RUN_CMD ${pkgs.rsync}/bin/rsync ''${VERBOSE_ARG:+-v} $rsyncArgs "$appsSrc" "$baseDir"
     '';
   };
 }
