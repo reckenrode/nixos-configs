4f5f8e2571ba04192c0126bc693a37943e6c0919
Randy Eckenrode <randy@largeandhighquality.com>
2021-03-29 23:43:07 -0400

    rsync fonts

--- common/users/reckenrode/x86_64-darwin/claudette/fonts.nix
+++ common/users/reckenrode/x86_64-darwin/claudette/fonts.nix
@@ -9,14 +9,11 @@
         pathsToLink = "/share/fonts/otf";
       };
     in lib.hm.dag.entryAfter [ "writeBoundary" ] ''
+      fontSrc="${fonts}/share/fonts/otf/"
       fontDir="$HOME/Library/Fonts/Home Manager Fonts"
-      if [ -d "$fontDir" ]; then
-        rm -rf "$fontDir"
-      fi
-      mkdir -p "$fontDir"
-      for font in ${fonts}/share/fonts/otf/*; do
-        $DRY_RUN_CMD cp ''${VERBOSE_ARG:+-v} -fHRL "$font" "$fontDir"
-      done
+      rsyncArgs="--archive --delete -L"
+      mkdir -p "$baseDir"
+      $DRY_RUN_CMD ${pkgs.rsync}/bin/rsync ''${VERBOSE_ARG:+-v} $rsyncArgs "$fontSrc" "$fontDir"
     '';
   };
 }
