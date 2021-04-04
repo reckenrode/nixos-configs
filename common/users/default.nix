af25f1c6bc758ffc5a26b858ea0f5f7d3a3e99d8
Randy Eckenrode <randy@largeandhighquality.com>
2021-04-04 17:41:45 -0400

    Update SSH keys

--- common/users/default.nix
+++ common/users/default.nix
@@ -11,6 +11,7 @@
     extraGroups = [ "wheel" ];
     openssh.authorizedKeys.keys = [
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIM+aydjZ/Yb8onZQ5OLyXZr18NchFZQcZh8yNEuK/wOM"
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIBP7kafg9acMfZFFX6yj6t7HTdGg+KfFkXSopu5ZySCj"
       "ecdsa-sha2-nistp256 AAAAE2VjZHNhLXNoYTItbmlzdHAyNTYAAAAIbmlzdHAyNTYAAABBBDTegWuvc3zHmVOMqcY8TJrLzWwS3W3ro4v7/782WoUHge2SuvLCinb8yyD+SgIg2OyEz8q+iXwNUaFOa7sTM20="
     ];
   };
