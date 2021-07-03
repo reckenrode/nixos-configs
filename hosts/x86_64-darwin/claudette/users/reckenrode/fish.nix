d20c5756fc1e56457f1c77612fac3b5e8f0124a3
Randy Eckenrode <randy@largeandhighquality.com>
2021-07-03 13:20:15 -0400

    Add fish_title to the shell config

--- hosts/x86_64-darwin/claudette/users/reckenrode/fish.nix
+++ hosts/x86_64-darwin/claudette/users/reckenrode/fish.nix
@@ -1,6 +1,16 @@
 { ... }:
 
 {
+  programs.fish.functions = {
+    fish_title = ''
+      set host (hostname -s)
+      set user $USER
+      set path (prompt_pwd)
+      set job $_
+      echo "$user@$host:$path  $job"
+    '';
+  };
+
   programs.fish.loginShellInit = let
     secretivePath = "$HOME/Library/Containers/com.maxgoedjen.Secretive.SecretAgent/Data/socket.ssh";
   in ''
