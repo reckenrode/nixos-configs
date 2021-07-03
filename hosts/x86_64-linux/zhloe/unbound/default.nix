e8472c692f6db7aa74469bb0b733ff30b957b9b3
Randy Eckenrode <randy@largeandhighquality.com>
2021-07-02 21:35:16 -0400

    Fix errors in Unbound configuration

--- hosts/x86_64-linux/zhloe/unbound/default.nix
+++ hosts/x86_64-linux/zhloe/unbound/default.nix
@@ -21,9 +21,9 @@
     settings = {
       server = {
         access-control = [
-          "192.168.238.1/24"
-          "fe80::/64"
-          "fda9:51fe:3bbf:c9f::/64"
+          "192.168.238.1/24 allow"
+          "fe80::/64 allow"
+          "fda9:51fe:3bbf:c9f::/64 allow"
         ];
         interface = [
           "192.168.238.1"
@@ -35,8 +35,8 @@
           "largeandhighquality.com. transparent"
         ];
         local-data = [
-          "jihli.infra.largeandhighquality.com AAAA fda9:51fe:3bbf:c9f:c665:16ff:fedd:7d5b"
-          "zhloe.infra.largeandhighquality.com AAAA fda9:51fe:3bbf:c9f:2e0:67ff:fe15:ced3"
+          ''"jihli.infra.largeandhighquality.com AAAA fda9:51fe:3bbf:c9f:c665:16ff:fedd:7d5b"''
+          ''"zhloe.infra.largeandhighquality.com AAAA fda9:51fe:3bbf:c9f:2e0:67ff:fe15:ced3"''
         ];
         tls-service-key = "${certPath}/key.pem";
         tls-service-pem = "${certPath}/fullchain.pem";
