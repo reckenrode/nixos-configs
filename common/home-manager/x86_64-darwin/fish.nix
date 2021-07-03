3c1ab3eaf8f9ba024fdc80df926bba4ac317ce53
Randy Eckenrode <randy@largeandhighquality.com>
2021-07-02 20:20:30 -0400

    Move XDG bin to the end of PATH

--- common/home-manager/x86_64-darwin/fish.nix
+++ common/home-manager/x86_64-darwin/fish.nix
@@ -8,12 +8,11 @@
   programs.fish.loginShellInit = let
     fishUserPaths = builtins.foldl' (a: b: "${a} ${b}") "" [
       "/run/current-system/sw/bin"
-      "$HOME/.local/bin"
       "$HOME/.nix-profile/bin"
       "/etc/profiles/per-user/$USER/bin"
     ];
   in ''
     set fish_user_paths ${fishUserPaths}
+    set --append PATH "$HOME/.local/bin"
   '';
-
 }
