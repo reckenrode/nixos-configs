a97d735ae7212158c8bc7304ee52f05ade10bd25
Randy Eckenrode <randy@largeandhighquality.com>
2021-07-02 20:24:02 -0400

    Align paths with NixOS

--- common/home-manager/x86_64-darwin/fish.nix
+++ common/home-manager/x86_64-darwin/fish.nix
@@ -7,9 +7,10 @@
 
   programs.fish.loginShellInit = let
     fishUserPaths = builtins.foldl' (a: b: "${a} ${b}") "" [
-      "/run/current-system/sw/bin"
       "$HOME/.nix-profile/bin"
       "/etc/profiles/per-user/$USER/bin"
+      "/nix/var/nix/profiles/default/bin"
+      "/run/current-system/sw/bin"
     ];
   in ''
     set fish_user_paths ${fishUserPaths}
