190c48e63b16b889482bc43bf927b27c7a1c43f6
Randy Eckenrode <randy@largeandhighquality.com>
2021-07-03 15:01:31 -0400

    Fix formatting of vscode.nix

--- hosts/x86_64-darwin/claudette/users/reckenrode/vscode.nix
+++ hosts/x86_64-darwin/claudette/users/reckenrode/vscode.nix
@@ -1,30 +1,30 @@
 { pkgs, ... }:
 
 {
-    programs.vscode = {
-      enable = true;
-      extensions = with pkgs.vscode-extensions; [
-        bbenoist.Nix
-        editorconfig.editorconfig
-        pkgs.unstable.vscode-extensions.ionide.ionide-fsharp # FIXME: once 21.11 lands
-        ms-dotnettools.csharp
-      ];
-      userSettings = {
-        "FSharp.dotnetRoot" = "${pkgs.dotnet-sdk_5}";
-        "editor.minimap.enabled" = false;
-        "editor.roundedSelection" = false;
-        "editor.rulers" = [ 100 ];
-        "explorer.confirmDelete" = false;
-        "explorer.confirmDragAndDrop" = false;
-        "extensions.autoCheckUpdates" = false;
-        "extensions.autoUpdate" = false;
-	"omnisharp.monoPath" = "${pkgs.mono}/bin/mono";
-	"omnisharp.useGlobalMono" = "always";
-        "telemetry.enableTelemetry" = false;
-        "update.mode" = "none";
-        "window.titleBarStyle" = "native";
-        "workbench.editor.tabCloseButton" = "left";
-        "workbench.colorTheme" = "Default Light+";
-      };
+  programs.vscode = {
+    enable = true;
+    extensions = with pkgs.vscode-extensions; [
+      bbenoist.Nix
+      editorconfig.editorconfig
+      pkgs.unstable.vscode-extensions.ionide.ionide-fsharp # FIXME: once 21.11 lands
+      ms-dotnettools.csharp
+    ];
+    userSettings = {
+      "FSharp.dotnetRoot" = "${pkgs.dotnet-sdk_5}";
+      "editor.minimap.enabled" = false;
+      "editor.roundedSelection" = false;
+      "editor.rulers" = [ 100 ];
+      "explorer.confirmDelete" = false;
+      "explorer.confirmDragAndDrop" = false;
+      "extensions.autoCheckUpdates" = false;
+      "extensions.autoUpdate" = false;
+      "omnisharp.monoPath" = "${pkgs.mono}/bin/mono";
+      "omnisharp.useGlobalMono" = "always";
+      "telemetry.enableTelemetry" = false;
+      "update.mode" = "none";
+      "window.titleBarStyle" = "native";
+      "workbench.editor.tabCloseButton" = "left";
+      "workbench.colorTheme" = "Default Light+";
     };
+  };
 }
