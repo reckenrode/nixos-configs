d1dca32e61e48126724a1dc6680d5fffecdd5113
Randy Eckenrode <randy@largeandhighquality.com>
2021-07-05 22:06:31 -0400

    Add a function to show diffs and cleanup switch

--- common/users/reckenrode/home-manager.nix
+++ common/users/reckenrode/home-manager.nix
@@ -3,15 +3,23 @@
 {
   home.packages = with pkgs; [
     neovim
-    nvd
     ripgrep
   ];
 
-  programs.fish.enable = true;
-
-  programs.fish.interactiveShellInit = ''
-    set -x EDITOR "${pkgs.neovim}/bin/nvim";
-  '';
+  programs.fish = {
+    enable = true;
+    interactiveShellInit = ''
+      set -x EDITOR "${pkgs.neovim}/bin/nvim";
+    '';
+    functions = {
+      darwin-rebuild = ''
+        command darwin-rebuild $argv
+        if string match -q switch -- $argv
+          ${pkgs.nvd}/bin/nvd diff /run/current-system result && rm result
+        end
+      '';
+    };
+  };
 
   home.stateVersion = "21.05";
 }
