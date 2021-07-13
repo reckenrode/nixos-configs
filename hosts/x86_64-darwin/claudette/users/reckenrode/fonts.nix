e6a373f91b9b9f2fde0fecc4cddc1cf5b8a53f7b
Randy Eckenrode <randy@largeandhighquality.com>
2021-07-13 10:50:57 -0400

    Create $fontDir instead of non-existant $baseDir

--- hosts/x86_64-darwin/claudette/users/reckenrode/fonts.nix
+++ hosts/x86_64-darwin/claudette/users/reckenrode/fonts.nix
@@ -12,7 +12,7 @@
       fontSrc="${fonts}/share/fonts/otf/"
       fontDir="$HOME/Library/Fonts/Home Manager Fonts"
       rsyncArgs="--archive --chmod=u-w --delete -cL"
-      mkdir -p "$baseDir"
+      mkdir -p "$fontDir"
       $DRY_RUN_CMD ${pkgs.findutils}/bin/find "$fontSrc" -maxdepth 1 -print0 | \
         ${pkgs.findutils}/bin/xargs -0 -n1 -P0 -I% \
         ${pkgs.rsync}/bin/rsync ''${VERBOSE_ARG:+-v} $rsyncArgs % "$fontDir"
