e455d40c126cae31cfe92bc0df10a6d1a5951403
Randy Eckenrode <randy@largeandhighquality.com>
2021-07-12 23:15:41 -0400

    Run rsync in parallel to improve performance with large apps

--- common/home-manager/x86_64-darwin/copyApplications.nix
+++ common/home-manager/x86_64-darwin/copyApplications.nix
@@ -13,7 +13,9 @@
       baseDir="$HOME/Applications/Home Manager Apps"
       rsyncArgs="--archive --chmod=u-w --delete -cL"
       mkdir -p "$baseDir"
-      $DRY_RUN_CMD ${pkgs.rsync}/bin/rsync ''${VERBOSE_ARG:+-v} $rsyncArgs "$appsSrc" "$baseDir"
+      $DRY_RUN_CMD ${pkgs.findutils}/bin/find "$appsSrc" -maxdepth 1 -print0 | \
+        ${pkgs.findutils}/bin/xargs -0 -n1 -P0 -I% \
+        ${pkgs.rsync}/bin/rsync ''${VERBOSE_ARG:+-v} $rsyncArgs % "$baseDir"
     '';
   };
 }
