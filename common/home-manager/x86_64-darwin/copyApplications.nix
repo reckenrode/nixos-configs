34087cb59904fab665c2fe641553bb4cb992cbfe
Randy Eckenrode <randy@largeandhighquality.com>
2021-07-14 01:58:57 -0400

    Disable parallel rsync
    It doesn’t handle uninstallations correctly, and it broke installation
    of RubyMine.

--- common/home-manager/x86_64-darwin/copyApplications.nix
+++ common/home-manager/x86_64-darwin/copyApplications.nix
@@ -13,9 +13,7 @@
       baseDir="$HOME/Applications/Home Manager Apps"
       rsyncArgs="--archive --chmod=u-w --delete -cL"
       $DRY_RUN_CMD ${pkgs.coreutils}/bin/mkdir -p "$baseDir"
-      $DRY_RUN_CMD ${pkgs.findutils}/bin/find "$appsSrc" -maxdepth 1 -print0 | \
-        ${pkgs.findutils}/bin/xargs -0 -n1 -P0 -I% \
-        ${pkgs.rsync}/bin/rsync ''${VERBOSE_ARG:+-v} $rsyncArgs % "$baseDir"
+      $DRY_RUN_CMD ${pkgs.rsync}/bin/rsync ''${VERBOSE_ARG:+-v} $rsyncArgs "$appsSrc" "$baseDir"
     '';
   };
 }
