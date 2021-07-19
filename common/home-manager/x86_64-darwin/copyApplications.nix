3ca2e814660782f50d06d08c32e65018c772b12d
Randy Eckenrode <randy@largeandhighquality.com>
2021-07-18 23:43:17 -0400

    Rewrite copyApplications
    Per the discussion in nix-community/home-manager#1341, only `Info.plist`
    needs to be copied over for Spotlight to find the app.  Also include
    some hacks to keep Visual Studio Code happy.

--- common/home-manager/x86_64-darwin/copyApplications.nix
+++ common/home-manager/x86_64-darwin/copyApplications.nix
@@ -9,11 +9,51 @@
         pathsToLink = "/Applications";
       };
     in lib.hm.dag.entryAfter [ "writeBoundary" ] ''
-      appsSrc="${apps}/Applications/"
+      appsSrc="${apps}/Applications"
       baseDir="$HOME/Applications/Home Manager Apps"
-      rsyncArgs="--archive --chmod=u-w --delete -cL"
-      $DRY_RUN_CMD ${pkgs.coreutils}/bin/mkdir -p "$baseDir"
-      $DRY_RUN_CMD ${pkgs.rsync}/bin/rsync ''${VERBOSE_ARG:+-v} $rsyncArgs "$appsSrc" "$baseDir"
+
+      # Need to make the directories writeable because `rm` will fail otherwise.
+      $DRY_RUN_CMD chmod -R +w ${VERBOSE_ARG:+-v} "$baseDir" \
+        && $DRY_RUN_CMD rm -rf ${VERBOSE_ARG:+-v} "$baseDir"
+
+      for app in "$appsSrc"/*; do
+	# Instead of symlinking into the home-manager buildEnv, symlink to the original app
+	# in the Nix store.  The buildEnv is just a convenient way of getting all the packages
+	# with .app bundles.
+        app=$(readlink "$app")
+
+        appName=$(basename "$app")
+        stagedApp="$baseDir/$appName"
+
+        # Copy Contents/Info.plist to make the app searchable by Spotlight
+        $DRY_RUN_CMD mkdir -p ${VERBOSE_ARG:+-v} "$stagedApp/Contents"
+        $DRY_RUN_CMD cp ${VERBOSE_ARG:+-v} \
+	  "$app/Contents/Info.plist" "$stagedApp/Contents/Info.plist"
+
+        # Do any apps even do this? Might as well check just in case ….
+        for file in "$app"/*; do
+          filename=$(basename "$file")
+          [ $filename != "Contents" ] && $DRY_RUN_CMD ln -s ${VERBOSE_ARG:+-v} "$file" "$stagedApp"
+        done
+
+        for file in "$app/Contents"/*; do
+          case "$file" in
+            # VS Code and (presumably) other Electron apps don’t work right if these are symlinks
+            "$app/Contents/MacOS" | "$app/Contents/Frameworks")
+              $DRY_RUN_CMD cp -r ${VERBOSE_ARG:+-v} "$file" "$stagedApp/Contents"
+              ;;
+            "$app/Contents/Info.plist")
+              ;;
+            *)
+              filename=$(basename "$file")
+              $DRY_RUN_CMD ln -s ${VERBOSE_ARG:+-v} "$file" "$stagedApp/Contents"
+              ;;
+           esac
+        done
+      done
+
+      $DRY_RUN_CMD find "$baseDir" -mindepth 1 -print0 | xargs -0 touch -ht 196912312359.59
+      $DRY_RUN_CMD chmod -R -w ${VERBOSE_ARG:+-v} "$baseDir"/*
     '';
   };
 }
