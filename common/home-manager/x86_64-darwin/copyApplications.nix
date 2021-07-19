6e5c42f70ff64dc88e0ca09964cc897730c41a52
Randy Eckenrode <randy@largeandhighquality.com>
2021-07-19 17:24:44 -0400

    Fix formatting in copyApplications.nix

--- common/home-manager/x86_64-darwin/copyApplications.nix
+++ common/home-manager/x86_64-darwin/copyApplications.nix
@@ -1,4 +1,5 @@
 { config, lib, pkgs, ... }:
+
 {
   home.activation = {
     copyApplications = let
@@ -9,10 +10,10 @@
       };
     in lib.hm.dag.entryAfter [ "writeBoundary" ] ''
       appsSrc="${apps}/Applications/"
-       baseDir="$HOME/Applications/Home Manager Apps"
-       rsyncArgs="--archive --checksum --chmod=u-w --copy-unsafe-links --delete"
-       $DRY_RUN_CMD mkdir -p "$baseDir"
-       $DRY_RUN_CMD ${pkgs.rsync}/bin/rsync ''${VERBOSE_ARG:+-v} $rsyncArgs "$appsSrc" "$baseDir"
+      baseDir="$HOME/Applications/Home Manager Apps"
+      rsyncArgs="--archive --checksum --chmod=u-w --copy-unsafe-links --delete"
+      $DRY_RUN_CMD mkdir -p "$baseDir"
+      $DRY_RUN_CMD ${pkgs.rsync}/bin/rsync ''${VERBOSE_ARG:+-v} $rsyncArgs "$appsSrc" "$baseDir"
      '';
    };
  }
