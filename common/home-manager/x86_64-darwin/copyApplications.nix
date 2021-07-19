74dfd441b889592cf697de204d1405e15ed048e8
Randy Eckenrode <randy@largeandhighquality.com>
2021-07-18 23:53:53 -0400

    Quote ${VERBOSE_ARG:+-v} properly

--- common/home-manager/x86_64-darwin/copyApplications.nix
+++ common/home-manager/x86_64-darwin/copyApplications.nix
@@ -13,8 +13,8 @@
       baseDir="$HOME/Applications/Home Manager Apps"
 
       # Need to make the directories writeable because `rm` will fail otherwise.
-      $DRY_RUN_CMD chmod -R +w ${VERBOSE_ARG:+-v} "$baseDir" \
-        && $DRY_RUN_CMD rm -rf ${VERBOSE_ARG:+-v} "$baseDir"
+      $DRY_RUN_CMD chmod -R +w ''${VERBOSE_ARG:+-v} "$baseDir" \
+        && $DRY_RUN_CMD rm -rf ''${VERBOSE_ARG:+-v} "$baseDir"
 
       for app in "$appsSrc"/*; do
 	# Instead of symlinking into the home-manager buildEnv, symlink to the original app
@@ -27,34 +27,35 @@
         stagedApp="$baseDir/$appName"
 
         # Copy Contents/Info.plist to make the app searchable by Spotlight
-        $DRY_RUN_CMD mkdir -p ${VERBOSE_ARG:+-v} "$stagedApp/Contents"
-        $DRY_RUN_CMD cp ${VERBOSE_ARG:+-v} \
+        $DRY_RUN_CMD mkdir -p ''${VERBOSE_ARG:+-v} "$stagedApp/Contents"
+        $DRY_RUN_CMD cp ''${VERBOSE_ARG:+-v} \
 	  "$app/Contents/Info.plist" "$stagedApp/Contents/Info.plist"
 
         # Do any apps even do this? Might as well check just in case ….
         for file in "$app"/*; do
           filename=$(basename "$file")
-          [ $filename != "Contents" ] && $DRY_RUN_CMD ln -s ${VERBOSE_ARG:+-v} "$file" "$stagedApp"
+          [ $filename != "Contents" ] \
+	    && $DRY_RUN_CMD ln -s ''${VERBOSE_ARG:+-v} "$file" "$stagedApp"
         done
 
         for file in "$app/Contents"/*; do
           case "$file" in
             # VS Code and (presumably) other Electron apps don’t work right if these are symlinks
             "$app/Contents/MacOS" | "$app/Contents/Frameworks")
-              $DRY_RUN_CMD cp -r ${VERBOSE_ARG:+-v} "$file" "$stagedApp/Contents"
+              $DRY_RUN_CMD cp -r ''${VERBOSE_ARG:+-v} "$file" "$stagedApp/Contents"
               ;;
             "$app/Contents/Info.plist")
               ;;
             *)
               filename=$(basename "$file")
-              $DRY_RUN_CMD ln -s ${VERBOSE_ARG:+-v} "$file" "$stagedApp/Contents"
+              $DRY_RUN_CMD ln -s ''${VERBOSE_ARG:+-v} "$file" "$stagedApp/Contents"
               ;;
            esac
         done
       done
 
       $DRY_RUN_CMD find "$baseDir" -mindepth 1 -print0 | xargs -0 touch -ht 196912312359.59
-      $DRY_RUN_CMD chmod -R -w ${VERBOSE_ARG:+-v} "$baseDir"/*
+      $DRY_RUN_CMD chmod -R -w ''${VERBOSE_ARG:+-v} "$baseDir"/*
     '';
   };
 }
