4cb7c8e1dd8fcbb2ce5532e158cfb945c795d76b
Randy Eckenrode <randy@largeandhighquality.com>
2021-07-31 09:18:52 -0400

    Let home-manager take care of installing fonts

--- hosts/x86_64-darwin/claudette/users/reckenrode/fonts.nix
+++ hosts/x86_64-darwin/claudette/users/reckenrode/fonts.nix
@@ -1,19 +1,12 @@
-{ config, lib, pkgs, ... }:
+{ pkgs, ... }:
 
+let
+  copyOtf = pkg: pkg.overrideAttrs (old: {
+      installPhase = ''
+        install -D -m 444 fonts/otf/* -t $out/share/fonts/otf
+      '';
+  });
+in
 {
-  home.activation = {
-    installFonts = let
-      fonts = pkgs.buildEnv {
-        name = "home-manager-fonts";
-        paths = with pkgs; [ alegreya alegreya-sans ];
-        pathsToLink = "/share/fonts/otf";
-      };
-    in lib.hm.dag.entryAfter [ "writeBoundary" ] ''
-      fontSrc="${fonts}/share/fonts/otf/"
-      fontDir="$HOME/Library/Fonts/Home Manager Fonts"
-      rsyncArgs="--archive --chmod=u-w --delete -cL"
-      $DRY_RUN_CMD ${pkgs.coreutils}/bin/mkdir -p "$fontDir"
-      $DRY_RUN_CMD ${pkgs.rsync}/bin/rsync ''${VERBOSE_ARG:+-v} $rsyncArgs "$fontSrc" "$fontDir"
-    '';
-  };
+  home.packages = map copyOtf [ pkgs.alegreya pkgs.alegreya-sans ];
 }
