b6edcad4fc3f282d24933ae95688b31e96073567
Randy Eckenrode <randy@largeandhighquality.com>
2021-10-13 13:06:03 -0400

    Add an activation script to clean up `gpg-agent`
    - `gpg-agent` will break if GNU PG is upgraded while it is running and
      remains running.  Work around it by killing it in the activation
      script.

--- hosts/x86_64-darwin/claudette/users/reckenrode/home-manager/gnupg.nix
+++ hosts/x86_64-darwin/claudette/users/reckenrode/home-manager/gnupg.nix
@@ -1,6 +1,12 @@
-{ pkgs, ... }:
+{ pkgs, lib, ... }:
 
 {
+  home.activation = {
+    cleanUpGpgAgent = lib.hm.dag.entryAfter [ "writeBoundary" ] ''
+      ${pkgs.gnupg}/bin/gpgconf --kill gpg-agent
+     '';
+   };
+
   home.file.".gnupg/gpg-agent.conf".text = ''
     pinentry-program ${pkgs.pinentry_mac}/Applications/pinentry-mac.app/Contents/MacOS/pinentry-mac
   '';
