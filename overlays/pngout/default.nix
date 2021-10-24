28d454d6b5ec46c118a82b9daf38e4ddb2340e66
Randy Eckenrode <randy@largeandhighquality.com>
2021-10-23 22:27:06 -0400

    Clean up `pngout` build

--- overlays/pngout/default.nix
+++ overlays/pngout/default.nix
@@ -1,17 +1,24 @@
 prev:
   prev.pngout.overrideAttrs (_: rec {
     name = "pngout-20200115";
+
     src = builtins.fetchurl {
       url = "http://www.jonof.id.au/files/kenutils/pngout-20200115-macos.zip";
       sha256 = "sha256-MnL6lH7q/BrACG4fFJNfnvoh0JClVeaJIlX+XIj2aG4=";
     };
-    nativeBuildInputs = [ prev.unzip ];
+    
+    buildInputs = [ prev.unzip ];
+    
     unpackPhase = ''
       unzip "$src"
     '';
+    
+    dontConfigure = true;
+    dontBuild = true;
+    dontFixup = true;
+
     installPhase = ''
       mkdir -p $out/bin
-      cp ${name}-macos/pngout $out/bin
-      /usr/bin/codesign -fs - $out/bin/pngout
+      install -m 755 ${name}-macos/pngout $out/bin
     '';
   })
