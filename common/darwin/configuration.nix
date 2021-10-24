87bd1b0d7b93ea9f49d9f7cabdaa4cb838b161f0
Randy Eckenrode <randy@largeandhighquality.com>
2021-10-24 12:58:12 -0400

    Force-override `nix.nixPath` on Darwin
    - Remove legacy/non-flake paths from `NIX_PATH`.

--- common/darwin/configuration.nix
+++ common/darwin/configuration.nix
@@ -1,9 +1,24 @@
-{ lib, pkgs, ... }:
+{ pkgs, lib, flake, ... }:
 
+# nix.nixPath based on the logic from `flake-utils-plus`
+# https://github.com/gytis-ivaskevicius/flake-utils-plus/blob/v1.3.0/lib/options.nix
+let
+  inherit (builtins) map;
+  inherit (lib) mkForce filterAttrs mapAttrsToList;
+  inherit (lib.trivial) pipe;
+
+  hasOutputs = _: flake: flake ? outputs;
+  hasPackages = _: flake: flake.outputs ? legacyPackages || flake.outputs ? packages;
+
+  flakesWithPkgs = pipe flake.inputs (map filterAttrs [ hasOutputs hasPackages ]);
+  nixPath = mapAttrsToList (name: _: "${name}=/etc/nix/inputs/${name}") flakesWithPkgs;
+in
 {
   imports = [
     ./nix-optimizations-darwin.nix
   ];
+
+  nix.nixPath = mkForce nixPath;
 
   services.nix-daemon.enable = true;
 
