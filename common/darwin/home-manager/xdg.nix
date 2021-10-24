4a8e51f0fa000835ddf8167f2b492a7fbb92031a
Randy Eckenrode <randy@largeandhighquality.com>
2021-10-24 16:21:37 -0400

    Define XDG variables on Darwin

--- common/darwin/home-manager/xdg.nix
+++ common/darwin/home-manager/xdg.nix
@@ -2,4 +2,14 @@
 
 {
   xdg.enable = true;
+
+  home.sessionVariables = {
+    XDG_DESKTOP_DIR = "${config.home.homeDirectory}/Desktop";
+    XDG_DOCUMENTS_DIR = "${config.home.homeDirectory}/Documents";
+    XDG_DOWNLOAD_DIR = "${config.home.homeDirectory}/Downloads";
+    XDG_MUSIC_DIR = "${config.home.homeDirectory}/Music";
+    XDG_PICTURES_DIR = "${config.home.homeDirectory}/Pictures";
+    XDG_PUBLICSHARE_DIR = "${config.home.homeDirectory}/Public";
+    XDG_VIDEOS_DIR = "${config.home.homeDirectory}/Movies";
+  };
 }
