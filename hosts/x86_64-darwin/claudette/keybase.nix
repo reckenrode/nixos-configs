c6eb72711477306861ad61bd3d336f59795b0364
Randy Eckenrode <randy@largeandhighquality.com>
2021-10-24 22:27:35 -0400

    Set Keybase log path

--- hosts/x86_64-darwin/claudette/keybase.nix
+++ hosts/x86_64-darwin/claudette/keybase.nix
@@ -4,7 +4,10 @@
   launchd.user.agents."keybase".serviceConfig = rec {
     ProgramArguments = [
       "/bin/sh" "-c"
-      "/bin/wait4path ${pkgs.keybase}/bin/keybase && exec ${pkgs.keybase}/bin/keybase -d service"
+      ''
+        /bin/wait4path ${pkgs.keybase}/bin/keybase && \
+          exec ${pkgs.keybase}/bin/keybase --log-file=/Users/reckenrode/Library/Logs/keybase.service.log service
+      ''
     ];
     EnvironmentVariables = {
       KEYBASE_LABEL = "keybase.service";
@@ -19,7 +22,10 @@
     launchd.user.agents."kbfs".serviceConfig = rec {
     ProgramArguments = [
       "/bin/sh" "-c"
-      "/bin/wait4path ${pkgs.kbfs}/bin/kbfsfuse && exec ${pkgs.kbfs}/bin/kbfsfuse -mount-type=none"
+      ''
+        /bin/wait4path ${pkgs.kbfs}/bin/kbfsfuse && \
+          exec ${pkgs.kbfs}/bin/kbfsfuse -log-file=/Users/reckenrode/Library/Logs/keybase.kbfs.log -mount-type=none
+      ''
     ];
     EnvironmentVariables = {
       KEYBASE_LABEL = "keybase.kbfs";
