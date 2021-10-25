78c342aa7ab683305ec0b2c4e91d37ef0c17c78c
Randy Eckenrode <randy@largeandhighquality.com>
2021-10-24 21:01:37 -0400

    Enable LFS on claudette

--- hosts/x86_64-darwin/claudette/users/reckenrode/home-manager/git.nix
+++ hosts/x86_64-darwin/claudette/users/reckenrode/home-manager/git.nix
@@ -9,6 +9,7 @@
       key = "01D754863A6D64EAAC770D26FBF19A982CCE0048";
       signByDefault = true;
     };
+    lfs.enable = true;
     extraConfig = {
       init = { defaultBranch = "main"; };
       credential.helper = "${pkgs.git}/bin/git-credential-osxkeychain";
