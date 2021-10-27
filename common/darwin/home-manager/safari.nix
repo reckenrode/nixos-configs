a381fe07b09b6a7f2caf6aa07d22b44adff6cb97
Randy Eckenrode <randy@largeandhighquality.com>
2021-10-27 00:59:31 -0400

    Set default tab settings for Safari 15

--- common/darwin/home-manager/safari.nix
+++ common/darwin/home-manager/safari.nix
@@ -7,8 +7,11 @@
     AutoFillMiscellaneousForms = false;
     AutoFillPasswords = false;
     AutoOpenSafeDownloads = false;
+    EnableNarrowTabs = true;
     IncludeDevelopMenu = true;
+    NeverUseBackgroundColorInToolbar = true;
     SearchProviderIdentifier = "com.duckduckgo";
     ShowOverlayStatusBar = true;
+    ShowStandaloneTabBar = true;
   };
 }
