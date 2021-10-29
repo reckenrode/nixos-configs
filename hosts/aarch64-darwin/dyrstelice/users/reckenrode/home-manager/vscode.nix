6955c588fdcfb778f34360063d3334a591e8ed7e
Randy Eckenrode <randy@largeandhighquality.com>
2021-10-29 08:27:25 -0400

    Reorganize VS Code configuration
    - Add vscode-fish;
    - Rename attrs after VS Code package names;
    - Move utility functions and definitions to the top; and
    - Inherit the package builder function.

--- hosts/aarch64-darwin/dyrstelice/users/reckenrode/home-manager/vscode.nix
+++ hosts/aarch64-darwin/dyrstelice/users/reckenrode/home-manager/vscode.nix
@@ -1,28 +1,9 @@
 { lib, pkgs, flakePkgs, unstablePkgs, ... }:
 
 let
+  inherit (pkgs.vscode-utils) buildVscodeMarketplaceExtension;
+
   debugserver = "${flakePkgs.debugserver}/bin/debugserver";
-
-  direnv = pkgs.vscode-utils.buildVscodeMarketplaceExtension {
-    mktplcRef = {
-      publisher = "cab404";
-      name = "vscode-direnv";
-      version = "1.0.0";
-      sha256 = "sha256-+nLH+T9v6TQCqKZw6HPN/ZevQ65FVm2SAo2V9RecM3Y=";
-    };
-  };
-
-  hledgerExtension = pkgs.vscode-utils.buildVscodeMarketplaceExtension {
-    mktplcRef = {
-      publisher = "mark-hansen";
-      name = "hledger-vscode";
-      version = "0.0.7";
-      sha256 = "sha256-whQaXrzDhVbDRlT7uCK5ORkxT4f1X4cWwSS1YOvL5xI=";
-    };
-  };
-
-  hledger-vscode = loadAfter [ "cab404.vscode-direnv" ] hledgerExtension;
-  rust-analyzer = loadAfter [ "cab404.vscode-direnv" ] pkgs.vscode-extensions.matklad.rust-analyzer;
 
   # Work around the lack of extension ordering in VS Code
   # See: https://github.com/Microsoft/vscode/issues/57481#issuecomment-910883638
@@ -36,21 +17,64 @@ let
       package.json | sponge package.json
     '';
   });
-  vscode-lldb = unstablePkgs.vscode-extensions.vadimcn.vscode-lldb.override {
+
+  cab404.direnv = buildVscodeMarketplaceExtension {
+    mktplcRef = {
+      publisher = "cab404";
+      name = "vscode-direnv";
+      version = "1.0.0";
+      sha256 = "sha256-+nLH+T9v6TQCqKZw6HPN/ZevQ65FVm2SAo2V9RecM3Y=";
+    };
+  };
+
+  bmalehorn.vscode-fish = buildVscodeMarketplaceExtension {
+    mktplcRef = {
+      publisher = "bmalehorn";
+      name = "vscode-fish";
+      version = "1.0.24";
+      sha256 = "sha256-q0TzKQFSqRX7KUAjOJPkUPk00YbdyCK8S8DfD3vsKcI=";
+    };
+  };
+
+  mark-hansen.hledger-vscode =
+    let
+      hledger-vscode = buildVscodeMarketplaceExtension {
+        mktplcRef = {
+          publisher = "mark-hansen";
+          name = "hledger-vscode";
+          version = "0.0.7";
+          sha256 = "sha256-whQaXrzDhVbDRlT7uCK5ORkxT4f1X4cWwSS1YOvL5xI=";
+        };
+      };
+    in
+    loadAfter [ "cab404.vscode-direnv" ] hledger-vscode;
+
+  matklad.rust-analyzer =
+    let
+      inherit (pkgs.vscode-extensions) matklad;
+    in
+    loadAfter [ "cab404.vscode-direnv" ] matklad.rust-analyzer;
+
+  vadimcn.vscode-lldb = unstablePkgs.vscode-extensions.vadimcn.vscode-lldb.override {
     inherit (pkgs) rustPlatform;
   };
 in
 {
   programs.vscode = {
     enable = true;
-    extensions = [
-      pkgs.vscode-extensions.bbenoist.Nix
-      pkgs.vscode-extensions.editorconfig.editorconfig
-      direnv
-      hledger-vscode
-      rust-analyzer
-      vscode-lldb
-    ];
+    extensions =
+      let
+        inherit (pkgs.vscode-extensions) bbenoist editorconfig;
+      in
+      [
+        bbenoist.Nix
+        bmalehorn.vscode-fish
+        cab404.direnv
+        editorconfig.editorconfig
+        mark-hansen.hledger-vscode
+        matklad.rust-analyzer
+        vadimcn.vscode-lldb
+      ];
     userSettings = {
       "editor.fontFamily" = "SF Mono, Menlo, Monaco, 'Courier New', monospace";
       "editor.minimap.enabled" = false;
