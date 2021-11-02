903604bcf7024afdc53aad5694e55cf860e9bd72
Randy Eckenrode <randy@largeandhighquality.com>
2021-11-02 08:34:13 -0400

    Clean up printer handling
    - Use `sed` to modify the contents of the PPD instead of replacing it
      completely.  This preserves any differences in the PPD depending on
      which operating system generates it.

--- hosts/aarch64-darwin/dyrstelice/cups/default.nix
+++ hosts/aarch64-darwin/dyrstelice/cups/default.nix
@@ -6,9 +6,10 @@ in {
   system.activationScripts.extraActivation.text = ''
     ppdFile="/etc/cups/ppd/HP_Color_LaserJet_Pro_M454dw.ppd"
     iconFile="/Library/Printers/Icons/M453dw.icns"
-    $DRY_RUN_CMD cp ''${VERBOSE_ARG:+-v} -fHRL "${ppd}" "$ppdFile"
-    $DRY_RUN_CMD cp ''${VERBOSE_ARG:+-v} -fHRL "${ppdIcon}" "$iconFile"
-    $DRY_RUN_CMD chmod ''${VERBOSE_ARG:+-v} 0644 "$ppdFile"
-    $DRY_RUN_CMD chmod ''${VERBOSE_ARG:+-v} 0644 "$iconFile"
+    $DRY_RUN_CMD ${pkgs.gnused}/bin/sed -Ez '
+      s|(\*cupsIdentifyActions: "display").*(\*APAcceptsMixedURF: True)|\1\n*APPrinterIconPath: "/Library/Printers/Icons/M453dw.icns"\n\2|
+      s|\*APSupplies: "[^"]*"|*APSupplies: "https://jihli.infra.largeandhighquality.com/#hId-pgConsumables"|
+    ' -i "$ppdFile" 2> /dev/null || true
+    $DRY_RUN_CMD install -m 644 -o root -g admin "${ppdIcon}" "$iconFile"
   '';
 }
