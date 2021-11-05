9bcd2b1a3134066da1e412dff2de56cbb4946258
Randy Eckenrode <randy@largeandhighquality.com>
2021-11-05 08:56:49 -0400

    Always load reset-raspberrypi

--- hosts/aarch64-linux/khloe/hardware-configuration.nix
+++ hosts/aarch64-linux/khloe/hardware-configuration.nix
@@ -8,8 +8,8 @@
     [ (modulesPath + "/installer/scan/not-detected.nix")
     ];
 
-  boot.initrd.availableKernelModules = [ "reset-raspberrypi" "xhci_pci" "usbhid" "usb_storage" "uas" ];
-  boot.initrd.kernelModules = [ ];
+  boot.initrd.availableKernelModules = [ "xhci_pci" "usbhid" "usb_storage" "uas" ];
+  boot.initrd.kernelModules = [ "reset-raspberrypi" ];
   boot.kernelModules = [ ];
   boot.extraModulePackages = [ ];
 
