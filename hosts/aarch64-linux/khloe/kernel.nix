056778ae80a0f8112cf2c8679c733a285d9a4821
Randy Eckenrode <randy@largeandhighquality.com>
2021-11-05 00:09:22 -0400

    Build reset-raspberrypi into the kernel

--- hosts/aarch64-linux/khloe/kernel.nix
+++ hosts/aarch64-linux/khloe/kernel.nix
@@ -1,5 +1,9 @@
 { lib, unstablePkgs, ... }:
 
 {
-  boot.kernelPackages = lib.mkForce unstablePkgs.linuxPackages_5_14;
+  boot.kernelPackages = lib.mkForce (unstablePkgs.linuxPackages_latest // {
+    kernel = unstablePkgs.linuxPackages_latest.kernel.overrideAttrs (_: {
+      extraConfig = "RESET_RASPBERRYPI y";
+    });
+  });
 }
