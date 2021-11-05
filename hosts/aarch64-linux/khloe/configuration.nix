631ed434d968e4f2aa058947cf581ad9b00af92a
Randy Eckenrode <randy@largeandhighquality.com>
2021-11-05 09:23:57 -0400

    Disable systemd-boot

--- hosts/aarch64-linux/khloe/configuration.nix
+++ hosts/aarch64-linux/khloe/configuration.nix
@@ -7,8 +7,8 @@
     ./systemd-networkd
   ];
 
-  boot.loader.systemd-boot.enable = true;
-  boot.loader.efi.canTouchEfiVariables = true;
+  boot.loader.systemd-boot.enable = false;
+  boot.loader.efi.canTouchEfiVariables = false;
 
   networking.hostName = "khloe";
 
