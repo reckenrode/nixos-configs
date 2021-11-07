7cf1a624a4f7e44d98c74fc3f5ec452aa5eaba5d
Randy Eckenrode <randy@largeandhighquality.com>
2021-11-07 09:18:19 -0500

    Disable the binary cache due to problems

--- common/linux/aarch64/configuration.nix
+++ common/linux/aarch64/configuration.nix
@@ -2,6 +2,4 @@
 
 {
   nix.extraOptions = "extra-platforms = armv7l-linux";
-  nix.binaryCaches = [ "https://app.cachix.org/cache/thefloweringash-armv7" ];
-  nix.binaryCachePublicKeys = [ "thefloweringash-armv7.cachix.org-1:v+5yzBD2odFKeXbmC+OPWVqx4WVoIVO6UXgnSAWFtso=" ];
 }
