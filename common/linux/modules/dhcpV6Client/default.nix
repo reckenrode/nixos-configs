b34952e5ec51c9570a71bbd614225d2fc2ee609b
Randy Eckenrode <randy@largeandhighquality.com>
2021-11-16 17:35:28 -0500

    Fix typos in module definition

--- common/linux/modules/dhcpV6Client/default.nix
+++ common/linux/modules/dhcpV6Client/default.nix
@@ -7,7 +7,7 @@ let
   inherit (pkgs) stdenv substituteAll;
 in
 {
-  options.services.dhcp6Client = {
+  options.services.dhcpV6Client = {
     openFirewall = lib.mkEnableOption "Open the firewall to allow DHCPv6 on the specified interfaces";
     interfaces = lib.mkOption {
       default = [];
@@ -19,14 +19,14 @@ in
   };
   config =
     let
-      inherit (config.services.dhcp6Client) interfaces;
+      inherit (config.services.dhcpV6Client) interfaces;
 
       generateFirewallRules = interface: readFile (pkgs.substituteAll {
         inherit interface;
-        src = ./dhcp6Client.nft;
+        src = ./dhcpV6Client.nft;
       });
     in
-    lib.mkIf config.services.dhcp6Client.openFirewall {
+    lib.mkIf config.services.dhcpV6Client.openFirewall {
       networking.nftables.ruleset = pipe interfaces [
         (map generateFirewallRules)
         (builtins.concatStringsSep "\n")
