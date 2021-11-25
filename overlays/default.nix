e0e2d0d9d8c1db54be71aa2a0ed4765255251fe9
Randy Eckenrode <randy@largeandhighquality.com>
2021-11-24 22:38:22 -0500

    Remove vestigial pngout import

--- overlays/default.nix
+++ overlays/default.nix
@@ -10,7 +10,6 @@ then
     kitty = import ./kitty prev;
     openra = prev.callPackage ./openra {};
     openttd = prev.callPackage ./openttd {};
-    pngout = import ./pngout prev;
     steam = prev.callPackage ./steam {};
     tesseract4 = prev.tesseract4.override {
       tesseractBase = import ./tesseract4 prev;
