19d80f3040466798653be29bf0c5141773372e79
Randy Eckenrode <randy@largeandhighquality.com>
2021-11-29 20:48:52 -0500

    Fix overlays on Linux

--- overlays/default.nix
+++ overlays/default.nix
@@ -2,13 +2,15 @@
 
 final: prev:
   let
-    inherit (builtins) listToAttrs;
+    inherit (builtins) listToAttrs pathExists;
     inherit (prev.lib.trivial) pipe;
 
     platform = prev.hostPlatform.parsed.kernel.name;
     overlays = lib.readDirNames ./${platform};
   in
-    pipe overlays [
+  if pathExists ./${platform}
+  then pipe overlays [
       (map (pkg: { name = pkg; value = import ./${platform}/${pkg} final prev; }))
       listToAttrs
     ]
+  else {}
