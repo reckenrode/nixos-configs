32d9f4e21d5e23dce983650be7bfa1d1460bc6e0
Randy Eckenrode <randy@largeandhighquality.com>
2021-12-03 22:11:22 -0500

    fixup! Update FF XIV settings management

--- hosts/aarch64-darwin/dyrstelice/users/reckenrode/home-manager/ffxiv.nix
+++ hosts/aarch64-darwin/dyrstelice/users/reckenrode/home-manager/ffxiv.nix
@@ -1,19 +1,20 @@
 { config, lib, pkgs, x86_64, ... }:
 
 let
-  ffxiv_path = "${config.home.homeDirectory}/Library/Application Support/FINAL FANTASY XIV ONLINE";
-  bottle_path = "Bottles/published_Final_Fantasy";
-  user_path = "drive_c/users/crossover";
-  docs_path = "${ffxiv_path}/${bottle_path}/${user_path}/My Documents";
-  settings_path = "${ffxiv_path}/Settings";
+  ffxivPath = "${config.home.homeDirectory}/Library/Application Support/FINAL FANTASY XIV ONLINE";
+  bottlePath = "Bottles/published_Final_Fantasy";
+  userPath = "drive_c/users/crossover";
+  docsPath = "${ffxivPath}/${bottlePath}/${userPath}/My Documents/My Games";
+  targetPath = "${docsPath}/FINAL FANTASY XIV - A Realm Reborn";
+  settingsPath = "${ffxivPath}/Settings";
 in
 {
   home.activation = {
     setUpFinalFantasyXIV =
       lib.hm.dag.entryAfter [ "writeBoundary" ] ''
-        $DRY_RUN_CMD mkdir -p ''${VERBOSE_ARG:+-v} "${settings_path}"
-        $DRY_RUN_CMD mkdir -p ''${VERBOSE_ARG:+-v} "${docs_path}"
-        $DRY_RUN_CMD ln -sfn ''${VERBOSE_ARG:+-v} "${settings_path}" "${docs_path}"
+        $DRY_RUN_CMD mkdir -p ''${VERBOSE_ARG:+-v} "${settingsPath}"
+        $DRY_RUN_CMD mkdir -p ''${VERBOSE_ARG:+-v} "${docsPath}"
+        $DRY_RUN_CMD ln -sfn ''${VERBOSE_ARG:+-v} "${settingsPath}" "${targetPath}"
      '';
    };
   home.packages = [ x86_64.flakePkgs.finalfantasyxiv ];
