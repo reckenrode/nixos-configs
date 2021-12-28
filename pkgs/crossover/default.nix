d4ca4a339436afdd93dae1fee5b605bdc8c51176
Randy Eckenrode <randy@largeandhighquality.com>
2021-12-28 11:35:52 -0500

    Update CrossOver derivation
    - Update MoltenVK to 1.1.6; and
    - Include an updated version of DXVK in `SharedSupport`.

--- pkgs/crossover/default.nix
+++ pkgs/crossover/default.nix
@@ -1,4 +1,4 @@
-{ lib, fetchurl, stdenv, unzip }:
+{ lib, fetchurl, fetchFromGitHub, stdenv, unzip, xz }:
 
 stdenv.mkDerivation rec {
   pname = "crossover";
@@ -14,18 +14,46 @@ stdenv.mkDerivation rec {
     hash = "sha256-e/tFudvMO0igTfgwN81kAwicLiGliUdCFEqr/AljZnc=";
   };
 
-  nativeBuildInputs = [ unzip ];
+  dxvk = fetchurl {
+    url = "https://github.com/marzent/dxvk/releases/download/v1.9.2-mac-async/dxvk-1.9.2-mac-async.zip";
+    hash = "sha256-2vOVR4b1ek1UdGRlIIGBLQd9XUqZLyVrxBnaufutfFg=";
+  };
+
+  moltenvk = fetchurl {
+    url = "https://github.com/Gcenx/MoltenVK/releases/download/v1.1.6/macos_dxvk_patched-1.1.6_2.tar.xz";
+    hash = "sha256-XwGEK/3AEKgo+LiRS2GTZNFlZsj+uudT2zZoEReMShU=";
+  };
+
+  buildInputs = [ unzip xz ];
+
+  unpackPhase = ''
+    unpackPhase
+    unzip ${dxvk}
+    xz -d < "${moltenvk}" | tar xf - --warning=no-timestamp
+  '';
+
+  buildPhase = ''
+    install -d -m755 Contents/SharedSupport/DXVK/
+    pushd ../dxvk-1.9.2-mac-async/x64/
+    install -m755 \
+      d3d10.dll d3d10_1.dll d3d10core.dll d3d11.dll d3d9.dll dxgi.dll \
+      ../../CrossOver.app/Contents/SharedSupport/DXVK/
+    popd
+    install -m755 ../Package/Release/MoltenVK/dylib/macOS/libMoltenVK.dylib \
+      Contents/SharedSupport/CrossOver/lib64/
+    install -m644 ${icon} Contents/Resources/CrossOver.icns
+    rm -rf "CrossOver.app/Contents/Resources/CrossOver CD Helper.app"
+  '';
 
   installPhase = ''
     mkdir -p $out/Applications
     cp -R ../*.app $out/Applications
-    rm -rf "$out/Applications/CrossOver.app/Contents/Resources/CrossOver CD Helper.app"
-    cp ${icon} $out/Applications/CrossOver.app/Contents/Resources/CrossOver.icns
   '';
 
   meta = {
     description = ''
-      Steam is the ultimate destination for playing, discussing, and creating games.
+      CrossOver is the easiest way to run many Microsoft applications on your Mac without a clunky
+      Windows emulator.
     '';
     homepage = "https://www.codeweavers.com/crossover";
     license = lib.licenses.unfree;
