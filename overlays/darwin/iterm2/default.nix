818df1ddf262203df2b87ce4fc4c92b0aef99a87
Randy Eckenrode <randy@largeandhighquality.com>
2022-01-08 11:33:41 -0500

    iterm2: Use darwin.sigtool

--- overlays/darwin/iterm2/default.nix
+++ overlays/darwin/iterm2/default.nix
@@ -1,6 +1,8 @@
 final: prev:
 
 prev.iterm2.overrideAttrs (old: {
+  buildInputs = [ prev.darwin.cctools prev.darwin.sigtool ];
+
   preBuild = ''
     ${prev.lib.optionalString (old ? preBuild) old.preBuild}
     sed -i "s/3.4.%(extra)s/${old.version}/" version.txt
@@ -9,17 +11,13 @@ prev.iterm2.overrideAttrs (old: {
   postFixup = ''
     ${prev.lib.optionalString (old ? postFixup) old.postFixup}
     ITERM_APP=$out/Applications/iTerm.app
-    /usr/bin/codesign --deep -s - -i com.googlecode.iterm2 \
-      --entitlements iTerm2.entitlements \
-      $ITERM_APP
-    /usr/bin/codesign -f -s - \
-      --entitlements plists/iTermServer.entitlements \
+    codesign -f -s - -i com.googlecode.iterm2 --entitlements iTerm2.entitlements \
+      $ITERM_APP/Contents/MacOS/iTerm2
+    codesign -f -s - --entitlements plists/iTermServer.entitlements \
       $ITERM_APP/Contents/MacOS/iTermServer
-    /usr/bin/codesign -f -s - \
-      --entitlements iTerm2SandboxedWorker/iTerm2SandboxedWorker.entitlements \
-      $ITERM_APP/Contents/XPCServices/iTerm2SandboxedWorker.xpc
-    /usr/bin/codesign -f -s - \
-      --entitlements pidinfo/pidinfo.entitlements \
-      $ITERM_APP/Contents/XPCServices/pidinfo.xpc
+    codesign -f -s - --entitlements iTerm2SandboxedWorker/iTerm2SandboxedWorker.entitlements \
+      $ITERM_APP/Contents/XPCServices/iTerm2SandboxedWorker.xpc/Contents/MacOS/iTerm2SandboxedWorker
+    codesign -f -s - --entitlements pidinfo/pidinfo.entitlements \
+      $ITERM_APP/Contents/XPCServices/pidinfo.xpc/Contents/MacOS/pidinfo
   '';
 })
