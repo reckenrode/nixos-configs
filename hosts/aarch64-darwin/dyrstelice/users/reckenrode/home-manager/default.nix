2cfa238d0719b3ca4451379c51961c6d606e5f76
Randy Eckenrode <randy@largeandhighquality.com>
2022-01-27 11:07:52 -0500

    Remove rarely used or unused programs
    - gomuks (just using the web version now); and
    - pngout (can be accessed via `nix shell`).

--- hosts/aarch64-darwin/dyrstelice/users/reckenrode/home-manager/default.nix
+++ hosts/aarch64-darwin/dyrstelice/users/reckenrode/home-manager/default.nix
@@ -14,40 +14,19 @@
   home.packages =
     let
       inherit (x86_64.flakePkgs) crossover wonderdraft;
-      inherit (x86_64.pkgs) openra pngout steam tiled;
+      inherit (x86_64.pkgs) openra steam tiled;
       inherit (flakePkgs) daisydisk netnewswire ocr-documents secretive verify-archive;
       inherit (pkgs) firefox-bin keybase waifu2x-converter-cpp openttd terminal-notifier;
-
-      gomuks = pkgs.gomuks.overrideAttrs (old: {
-        postInstall = ''
-          cp -r ${
-            pkgs.makeDesktopItem {
-              name = "net.maunium.gomuks.desktop";
-              exec = "@out@/bin/gomuks";
-              terminal = "true";
-              desktopName = "Gomuks";
-              genericName = "Matrix client";
-              categories = "Network;Chat";
-              comment = old.meta.description;
-            }
-          }/* $out/
-          substituteAllInPlace $out/share/applications/*
-          wrapProgram $out/bin/gomuks \
-            --prefix PATH : "${lib.makeBinPath [ terminal-notifier ]}"
-        '';
-      });
     in
     [
       crossover
       daisydisk
       firefox-bin
-      gomuks
       keybase
       netnewswire
       ocr-documents
       openra
       openttd
-      pngout
       secretive
       steam
       tiled
