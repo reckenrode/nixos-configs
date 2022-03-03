db2dc4ae834e2e15c906acd315153bf50bd0c990
Randy Eckenrode <randy@largeandhighquality.com>
2022-03-02 21:48:16 -0500

    Install Firefox on iMac

--- hosts/x86_64-darwin/iMac/configuration.nix
+++ hosts/x86_64-darwin/iMac/configuration.nix
@@ -1,8 +1,13 @@
-{ ... }:
+{ pkgs
+, ...
+}:
 
 {
   networking.hostName = "iMac";
   security.pam.enableSudoTouchIdAuth= true;
   system.homePrinterFixup = true;
   programs.zsh.enable = true;
+  environment.systemPackages = [
+    pkgs.firefox-bin
+  ];
 }
