2acd7b0699fd57628deb7b8855b4d5f0ea8f8cb1
Randy Eckenrode <randy@largeandhighquality.com>
2022-03-02 21:58:11 -0500

    Fix missing app directory failure

--- common/darwin/home-manager/copyApplications.nix
+++ common/darwin/home-manager/copyApplications.nix
@@ -4,10 +4,12 @@
   home.activation = {
     copyApplications = lib.hm.dag.entryAfter [ "writeBoundary" ] ''
       appsSrc="$newGenPath/home-path/Applications/"
-      baseDir="$HOME/Applications/Home Manager Apps"
-      rsyncArgs="--archive --checksum --chmod=-w --copy-unsafe-links --delete"
-      $DRY_RUN_CMD mkdir -p "$baseDir"
-      $DRY_RUN_CMD ${pkgs.rsync}/bin/rsync ''${VERBOSE_ARG:+-v} $rsyncArgs "$appsSrc" "$baseDir"
+      if [ -d "$appsSrc" ]; then
+        baseDir="$HOME/Applications/Home Manager Apps"
+        rsyncArgs="--archive --checksum --chmod=-w --copy-unsafe-links --delete"
+        $DRY_RUN_CMD mkdir -p "$baseDir"
+        $DRY_RUN_CMD ${pkgs.rsync}/bin/rsync ''${VERBOSE_ARG:+-v} $rsyncArgs "$appsSrc" "$baseDir"
+      fi
     '';
   };
 }
