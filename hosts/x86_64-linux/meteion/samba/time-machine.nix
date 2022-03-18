a0fbd5fc12ac6bb2342914354806c96f5b1bf242
Randy Eckenrode <randy@largeandhighquality.com>
2022-03-18 01:19:13 -0400

    meteion: fix mdns settings

--- hosts/x86_64-linux/meteion/samba/time-machine.nix
+++ hosts/x86_64-linux/meteion/samba/time-machine.nix
@@ -5,7 +5,7 @@
 {
   services.avahi = {
     enable = true;
-    publish.userServices = true;
+    publish.enable = true;
     extraServiceFiles = {
       time-machine = ''
         <?xml version="1.0" standalone='no'?>
@@ -15,17 +15,6 @@
           <service>
             <type>_smb._tcp</type>
             <port>445</port>
-            <host-name>meteion.infra.largeandhighquality.com</host-name>
-          </service>
-          <service>
-            <type>_device-info._tcp</type>
-            <port>0</port>
-            <txt-record>model=MacSamba@ECOLOR=226,226,224</txt-record>
-          </service>
-          <service>
-            <type>_adisk._tcp</type>
-            <txt-record>sys=waMa=0,adVF=0x100</txt-record>
-            <txt-record>dk0=adVN=TimeMachine,adVF=0x82</txt-record>
             <host-name>meteion.infra.largeandhighquality.com</host-name>
           </service>
         </service-group>
