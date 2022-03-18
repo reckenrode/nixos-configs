9bfb4f5708416774227481fa386e667df3663b79
Randy Eckenrode <randy@largeandhighquality.com>
2022-03-18 01:26:13 -0400

    meteion: add a time machine user

--- hosts/x86_64-linux/meteion/samba/time-machine.nix
+++ hosts/x86_64-linux/meteion/samba/time-machine.nix
@@ -24,14 +24,25 @@
 
   services.samba.shares.time-machine = {
     path = "/srv/time-machine";
-    "valid users" = "reckenrode weiweilin";
+    "valid users" = "time-machine";
     public = "no";
     writeable = "yes";
+    "force user" = "time-machine";
     "force group" = "time-machine";
     "guest ok" = "no";
     "fruit:time machine" = "yes";
     "fruit:time machine max size" = "4TB";
   };
 
-  users.groups.time-machine = { };
+  users = {
+    groups.time-machine = { };
+    users.time-machine = {
+      isSystemUser = true;
+      description = "Time Machine Backups";
+      group = "time-machine";
+      home = "/var/empty";
+      createHome = false;
+      shell = pkgs.shadow;
+    };
+  };
 }
