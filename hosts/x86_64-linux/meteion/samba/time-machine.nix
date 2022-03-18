cbf2e7f6266d08005c39bff722189a306578aa00
Randy Eckenrode <randy@largeandhighquality.com>
2022-03-18 08:22:56 -0400

    meteion: let Samba advertise the time machine volume

--- hosts/x86_64-linux/meteion/samba/time-machine.nix
+++ hosts/x86_64-linux/meteion/samba/time-machine.nix
@@ -7,26 +7,7 @@
     enable = true;
     publish = {
       enable = true;
-      addresses = true;
-    };
-    extraServiceFiles = {
-      time-machine = ''
-        <?xml version="1.0" standalone='no'?>
-        <!DOCTYPE service-group SYSTEM "avahi-service.dtd">
-        <service-group>
-          <name replace-wildcards="yes">%h</name>
-          <service>
-            <type>_smb._tcp</type>
-            <port>445</port>
-            <host-name>meteion.infra.largeandhighquality.com</host-name>
-          </service>
-          <service>
-            <type>_device-info._tcp</type>
-            <port>0</port>
-            <txt-record>model=RackMac</txt-record>
-          </service>
-        </service-group>
-      '';
+      userServices = true;
     };
   };
 
