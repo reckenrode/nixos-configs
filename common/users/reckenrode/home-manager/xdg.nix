406aae68ead33c5c08af4d6808d0b34e06e11df7
Randy Eckenrode <randy@largeandhighquality.com>
2022-04-18 00:26:31 -0400

    Drop redundant XDG path setting

--- common/users/reckenrode/home-manager/xdg.nix
+++ common/users/reckenrode/home-manager/xdg.nix
@@ -1,10 +1,9 @@
-{ config, pkgs, ... }:
+{ config, ... }:
 
 {
   xdg.enable = true;
 
   home.sessionVariables = {
-    XDG_STATE_HOME = "${config.home.homeDirectory}/.local/state";
     XDG_DESKTOP_DIR = "${config.home.homeDirectory}/Desktop";
     XDG_DOCUMENTS_DIR = "${config.home.homeDirectory}/Documents";
     XDG_DOWNLOAD_DIR = "${config.home.homeDirectory}/Downloads";
