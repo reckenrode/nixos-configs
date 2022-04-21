437693e6c2bffb51a30cbffc1ce3e552124d41c6
Randy Eckenrode <randy@largeandhighquality.com>
2022-04-21 08:39:40 -0400

    darwin: switch to `nix.gc`

--- common/darwin/nix-optimizations-darwin.nix
+++ common/darwin/nix-optimizations-darwin.nix
@@ -1,25 +1,6 @@
 { config, lib, pkgs, ... }:
 
 {
-  launchd.daemons."nix-collect-garbage".serviceConfig = {
-    ProgramArguments = [
-      "/bin/sh"
-      "-c"
-      ''
-        /bin/wait4path ${config.nix.package}/bin/nix-collect-garbage && \
-          exec ${config.nix.package}/bin/nix-collect-garbage --delete-older-than 30d
-      ''
-    ];
-    StartCalendarInterval = [
-      {
-        Weekday = 0;
-        Hour = 2;
-        Minute = 0;
-      }
-    ];
-    StandardErrorPath = "/var/log/nix-store.log";
-    StandardOutPath = "/var/log/nix-store.log";
-  };
   # While it’s possible to set `auto-optimise-store` in `nix.conf`, it sometimes causes problems
   # on Darwin.  Run a job periodically to optimise the store.
   launchd.daemons."nix-store-optimise".serviceConfig = {
@@ -39,5 +20,11 @@
     ];
     StandardErrorPath = "/var/log/nix-store.log";
     StandardOutPath = "/var/log/nix-store.log";
+  };
+
+  nix.gc = {
+    automatic = true;
+    interval = { Weekday = 0; Hour = 2; Minute = 0; };
+    options = "--delete-older-than 30d";
   };
 }
