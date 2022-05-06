ef4b22b82712743717c6efc0c55abf16cfc2c046
Randy Eckenrode <randy@largeandhighquality.com>
2022-05-06 18:18:01 -0400

    xdg: set XDG_CACHE_HOME for GUI apps

--- common/darwin/home-manager/xdg.nix
+++ common/darwin/home-manager/xdg.nix
@@ -5,4 +5,15 @@
 {
   # Move Cache home to ~/Library/Caches, so it plays nicely with backups
   xdg.cacheHome = "${config.home.homeDirectory}/Library/Caches";
+
+  launchd.agents.xdg_cache_home = {
+    enable = true;
+    config = {
+      Program = "/bin/launchctl";
+      ProgramArguments = [ "/bin/launchctl" "setenv" "XDG_CACHE_HOME" config.xdg.cacheHome ];
+      RunAtLoad = true;
+      StandardErrorPath = "/dev/null";
+      StandardOutPath = "/dev/null";
+    };
+  };
 }
