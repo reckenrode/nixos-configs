5119c30b55e8569e8360f2f880a5cddd7f99aa05
Randy Eckenrode <randy@largeandhighquality.com>
2022-06-08 08:00:53 -0400

    pkgs: allow the channel to be specified

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -1,21 +1,19 @@
 { lib, channels }:
 
 let
-  inherit (builtins) fromJSON listToAttrs readFile;
+  inherit (builtins) attrNames fromJSON listToAttrs readFile;
   inherit (channels.nixpkgs.lib.trivial) pipe;
 
   flakePkgs = lib.readDirNames ./.;
   unstablePkgs = pipe ./unstable-packages.json [
     readFile
     fromJSON
-    (map (attr: { name = attr; value = attr; }))
-    listToAttrs
   ];
 in
 pipe flakePkgs [
   (map (pkg:
     if unstablePkgs ? "${pkg}"
-    then { name = pkg; value = channels.nixpkgs-unstable.callPackage ./${pkg} {}; }
+    then { name = pkg; value = channels.${unstablePkgs.${pkg}}.callPackage ./${pkg} {}; }
     else { name = pkg; value = channels.nixpkgs.callPackage ./${pkg} {}; }))
   listToAttrs
 ]
