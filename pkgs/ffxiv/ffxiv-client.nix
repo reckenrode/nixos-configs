6ebadff9f08c0c50f795085b6127389ac8d8485b
Randy Eckenrode <randy@largeandhighquality.com>
2022-07-06 20:21:45 -0400

    ffxiv: 1.0.7 → 1.0.8

--- pkgs/ffxiv/ffxiv-client.nix
+++ pkgs/ffxiv/ffxiv-client.nix
@@ -7,15 +7,14 @@
 
 stdenvNoCC.mkDerivation rec {
   pname = "ffxiv";
-  version = "1.0.7";
+  version = "1.0.8";
 
   # The Windows installer is a 32-bit app, which won’t run on Darwin because WoW64 is not yet
   # supported there with upstream Wine. The Mac client also has Bink-encoded video files that are
   # needed because the WMV-encoded ones in the Windows client don’t work with Wine by default.
-  # The 1.0.5 version was pulled off Square Enix CDN and replaced with the unreleased 1.0.7 version for macOS.
   src = fetchurl {
     url = "https://mac-dl.ffxiv.com/cw/finalfantasyxiv-${version}.zip";
-    hash = "sha256-ogUlzOhyIuapu9n+H4GKOsoacDh6tGb5XM04kWuXtp4=";
+    sha256 = "sha256-CBzl0gxKyQpkyu8kTZaFYFEJQC0vMlObRBGGOLOxmNA=";
   };
 
   nativeBuildInputs = [ unzip ];
