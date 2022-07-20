226f6ed909cfb9fa6d55433723b836a850aad309
Randy Eckenrode <randy@largeandhighquality.com>
2022-07-20 19:52:11 -0400

    Use nvd on Dariwn instead of `nix diff-closures`

--- common/darwin/diff-closures.nix
+++ common/darwin/diff-closures.nix
@@ -1,8 +1,14 @@
-{ config, ... }:
+{ lib, pkgs, ... }:
 
+let
+  nvd = "${lib.getBin pkgs.nvd}/bin/nvd";
+  find = "${lib.getBin pkgs.findutils}/bin/find";
+  readlink = "${lib.getBin pkgs.coreutils}/bin/readlink";
+in
 {
   system.activationScripts.postActivation.text = ''
-    ${config.nix.package}/bin/nix store --experimental-features 'nix-command' diff-closures \
-      /run/current-system "$systemConfig"
+    old_generation=$(${find} /nix/var/nix/profiles -lname $(${readlink} -f /run/current-system))
+    new_generation=$(${find} /nix/var/nix/profiles -lname $(${readlink} -f "$systemConfig"))
+    ${nvd} diff "$old_generation" "$new_generation"
   '';
 }
