51f833f4da6750ef27daed18d96904de57860860
Randy Eckenrode <randy@largeandhighquality.com>
2022-11-08 21:19:26 -0500

    meteion: open DHCPv6 client on wlan

--- hosts/x86_64-linux/meteion/systemd-networkd.nix
+++ hosts/x86_64-linux/meteion/systemd-networkd.nix
@@ -3,20 +3,10 @@
 {
   services.dhcpV6Client = {
     openFirewall = true;
-    interfaces = [ "enp3s0" ];
+    interfaces = [ "wlan0" ];
   };
 
   services.resolved.dnssec = "true";
-
-  systemd.network.networks.lan = {
-    enable = true;
-    matchConfig.Name = "enp3s0";
-    networkConfig = {
-      DHCP = "ipv4";
-      IPv6PrivacyExtensions = true;
-      IPv6AcceptRA = true;
-    };
-  };
 
   systemd.network.networks.wlan = {
     enable = true;
