8dd6ba16258e93f4096b8bb4599a4b2e604ad642
Randy Eckenrode <randy@largeandhighquality.com>
2023-04-19 21:19:33 -0400

    meteion: use iwd 2.3

--- hosts/meteion/systemd-networkd.nix
+++ hosts/meteion/systemd-networkd.nix
@@ -1,6 +1,11 @@
 # SPDX-License-Identifier: MIT
 
+{ lib, pkgs, inputs, ... }:
+
 {
+  # FIXME: Switch to the stable package once 23.05 is released.
+  networking.wireless.iwd.package = inputs.nixpkgs-unstable.legacyPackages.${pkgs.system}.iwd;
+
   systemd.network.networks.wlan = {
     enable = true;
     matchConfig.Name = "wlan";
