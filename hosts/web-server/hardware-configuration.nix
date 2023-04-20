73d5b60bc6b681c77d6f7995740e8d171af9ba56
Randy Eckenrode <randy@largeandhighquality.com>
2023-04-19 22:38:55 -0400

    web-server: update hardware config

--- hosts/web-server/hardware-configuration.nix
+++ hosts/web-server/hardware-configuration.nix
@@ -4,10 +4,7 @@
 { config, lib, pkgs, modulesPath, ... }:
 
 {
-  imports =
-    [
-      (modulesPath + "/profiles/qemu-guest.nix")
-    ];
+  imports = [ (modulesPath + "/profiles/qemu-guest.nix") ];
 
   boot.initrd.availableKernelModules = [ "virtio_pci" "virtio_scsi" "ahci" "sd_mod" ];
   boot.initrd.kernelModules = [ ];
@@ -27,6 +24,10 @@
     };
 
   swapDevices =
-    [{ device = "/dev/sdb"; }];
+    [
+      { device = "/dev/sdb"; }
+    ];
 
+  nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
+  hardware.cpu.amd.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
 }
