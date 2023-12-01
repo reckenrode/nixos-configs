8cc1e80baf9e674878b6d6c5fd1cd27478fa91f0
Randy Eckenrode <randy@largeandhighquality.com>
2023-11-30 20:51:03 -0500

    Update OpenSSH service configuration for 23.11

--- modules/by-name/op/openssh/nixos-module.nix
+++ modules/by-name/op/openssh/nixos-module.nix
@@ -21,20 +21,22 @@ in
 
     services.openssh = {
       extraConfig = "HostKeyAlgorithms -ssh-rsa";
-      kexAlgorithms = [
-        "curve25519-sha256"
-        "curve25519-sha256@libssh.org"
-        "diffie-hellman-group14-sha256"
-        "diffie-hellman-group16-sha512"
-        "diffie-hellman-group18-sha512"
-        "diffie-hellman-group-exchange-sha256"
-      ];
-      macs = [
-        "hmac-sha2-512-etm@openssh.com"
-        "hmac-sha2-256-etm@openssh.com"
-        "umac-128-etm@openssh.com"
-      ];
-      passwordAuthentication = false;
+      settings = {
+        KexAlgorithms = [
+          "curve25519-sha256"
+          "curve25519-sha256@libssh.org"
+          "diffie-hellman-group14-sha256"
+          "diffie-hellman-group16-sha512"
+          "diffie-hellman-group18-sha512"
+          "diffie-hellman-group-exchange-sha256"
+        ];
+        Macs = [
+          "hmac-sha2-512-etm@openssh.com"
+          "hmac-sha2-256-etm@openssh.com"
+          "umac-128-etm@openssh.com"
+        ];
+        PasswordAuthentication = false;
+      };
     };
   };
 }
