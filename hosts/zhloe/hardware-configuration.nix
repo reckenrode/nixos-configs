6dbb4ee5dfedad25ba2e92526516843d87a1a2c1
Randy Eckenrode <randy@largeandhighquality.com>
2025-01-19 12:01:12 -0500

    zhloe: drop CPU frequency governor setting

--- hosts/zhloe/hardware-configuration.nix
+++ hosts/zhloe/hardware-configuration.nix
@@ -38,5 +38,4 @@
 
   nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
   hardware.cpu.intel.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
-  powerManagement.cpuFreqGovernor = lib.mkDefault "ondemand";
 }
