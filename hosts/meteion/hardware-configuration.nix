b2472a139b144fa2effe4f1d0557b8be172feefd
Randy Eckenrode <randy@largeandhighquality.com>
2025-01-19 12:01:12 -0500

    meteion: drop CPU frequency governor setting

--- hosts/meteion/hardware-configuration.nix
+++ hosts/meteion/hardware-configuration.nix
@@ -80,5 +80,4 @@
 
   nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
   hardware.cpu.amd.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
-  powerManagement.cpuFreqGovernor = lib.mkDefault "ondemand";
 }
