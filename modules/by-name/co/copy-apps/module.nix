6acbc24aa5b8d7641f7da0527ead46e8740a3b36
Randy Eckenrode <randy@largeandhighquality.com>
2025-04-16 21:08:26 -0400

    copyApps: link `Contents` instead of copying the whole app

--- modules/by-name/co/copy-apps/module.nix
+++ modules/by-name/co/copy-apps/module.nix
@@ -27,19 +27,27 @@ let
     + ''
       appsSrc="${appsSrc}"
       if [ -d "$appsSrc" ]; then
+        workDir=$(mktemp -d)
+        # shellcheck disable=SC2064
+        trap "rm -rf -- '$workDir'" EXIT
+
+        while IFS= read -r -d "" app; do
+          mkdir -p "$workDir/$app"
+          ln -s "$(realpath "$appsSrc/$app/Contents")" "$workDir/$app/Contents"
+        done < <(${lib.getExe pkgs.findutils} "$appsSrc" -name '*.app' -and -not -path '*.app/*.app' -printf '%P\0')
+
         baseDir="${baseDir}"
         rsyncFlags=(
-          --archive
-          --checksum
-          --chmod=-w
-          --copy-unsafe-links
-          --delete
-          --no-group
-          --no-owner
+          "--archive"
+          "--checksum"
+          "--chmod=-w,uog+rx"
+          "--delete"
+          "--no-group"
+          "--no-owner"
         )
         $DRY_RUN_CMD mkdir -p "$baseDir"
         $DRY_RUN_CMD ${lib.getBin pkgs.rsync}/bin/rsync \
-          ''${VERBOSE_ARG:+-v} "''${rsyncFlags[@]}" "$appsSrc/" "$baseDir"
+          ''${VERBOSE_ARG:+-v} "''${rsyncFlags[@]}" "$workDir/" "$baseDir"
       fi
     '';
 
