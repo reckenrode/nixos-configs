cf485e679f63b193db7d0826bc0b6694456dbc42
Randy Eckenrode <randy@largeandhighquality.com>
2025-07-07 20:19:50 -0400

    meteion: enable ethernet

--- hosts/meteion/systemd-networkd.nix
+++ hosts/meteion/systemd-networkd.nix
@@ -7,6 +7,16 @@
 }:
 
 {
+  systemd.network.networks.lan = {
+    enable = true;
+    matchConfig.Type = "ether";
+    networkConfig = {
+      DHCP = "ipv4";
+      IPv6PrivacyExtensions = true;
+      IPv6AcceptRA = true;
+    };
+  };
+
   systemd.network.networks.wlan = {
     enable = true;
     matchConfig.Type = "wlan";
