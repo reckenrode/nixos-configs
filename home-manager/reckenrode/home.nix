70f78772f65ddf573cd738e7180913813c7c88d1
Randy Eckenrode <randy@largeandhighquality.com>
2026-01-03 22:35:18 -0500

    home-manager/reckenrode: update for 25.11

--- home-manager/reckenrode/home.nix
+++ home-manager/reckenrode/home.nix
@@ -45,7 +45,7 @@ in
   home = {
     username = "reckenrode";
     homeDirectory = "/Users/reckenrode";
-    stateVersion = "24.11";
+    stateVersion = "25.11";
   };
 
   home.packages =
@@ -65,16 +65,18 @@ in
 
   programs.git = {
     enable = true;
-    userEmail = "randy@largeandhighquality.com";
-    userName = "Randy Eckenrode";
-    signing = {
-      key = "01D754863A6D64EAAC770D26FBF19A982CCE0048";
-      signByDefault = true;
-    };
-    extraConfig = {
+    settings = {
+      user = {
+        email = "randy@largeandhighquality.com";
+        name  = "Randy Eckenrode";
+      };
       init.defaultBranch = "main";
       credential.helper = "${lib.getBin pkgs.git}/bin/git-credential-osxkeychain";
       url."git@github.com:".pushInsteadOf = "https://github.com/";
+    };
+    signing = {
+      key = "01D754863A6D64EAAC770D26FBF19A982CCE0048";
+      signByDefault = true;
     };
   };
 
@@ -106,6 +108,7 @@ in
 
   programs.ssh = {
     enable = true;
+    enableDefaultConfig = false;
     matchBlocks =
       let
         secretive = "~/Library/Containers/com.maxgoedjen.Secretive.SecretAgent/Data/socket.ssh";
